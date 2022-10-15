63493c09bf2261c89596747b1bff336e68f24377
Zulfiqor <82012396+mzulfiqor@users.noreply.github.com>
2022-10-15 16:20:34 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const bodyParser = require('body-parser')
 require("./src/database/index.js")
 const routes = require('./src/routes/index.js');
 
@@ -10,6 +11,7 @@ const port = 4000
 app.use(cors())
 app.use(morgan("tiny"))
 app.use(express.json())
+app.use(bodyParser.json());
 app.use("/book", routes)
 
 app.listen(port, () => {
